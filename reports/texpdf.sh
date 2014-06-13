650866f73aff6bb766d997d88472a5ce8eaebe99
tyage <namatyage@gmail.com>
2014-06-13 09:20:40 +0900

    レポート1を書いたよっ！

--- reports/texpdf.sh
+++ reports/texpdf.sh
@@ -4,5 +4,9 @@ DVI_PATH=`echo $1 | sed -e "s/^\(.*\)\.tex$/\1.dvi/"`
 PDF_PATH=`echo $1 | sed -e "s/^\(.*\)\.tex$/\1.pdf/"`
 
 platex $1
-dvipdfmx "${DVI_PATH}"
-evince "${PDF_PATH}"
+dvipdfmx $DVI_PATH
+if [ -x "`which evince`" ]; then
+  evince $PDF_PATH
+else
+  open $PDF_PATH
+fi
